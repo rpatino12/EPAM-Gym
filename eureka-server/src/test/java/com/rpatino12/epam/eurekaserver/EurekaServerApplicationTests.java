8bb1336379b32087cde12640fbb684fc88f48b6c
Ricardo Patino <rpatino12@outlook.com>
2024-05-14 10:30:53 -0500

    Set up the Eureka Server

---
+++ eureka-server/src/test/java/com/rpatino12/epam/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpatino12.epam.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

b549602be91e320c8a1fd2a992c935e265675404
Ricardo Patino <rpatino12@outlook.com>
2024-05-14 10:43:01 -0500

    Register microservices in the Eureka server

--- gym-service/src/main/java/com/rpatino12/epam/gym/GymApplication.java
+++ gym-service/src/main/java/com/rpatino12/epam/gym/GymApplication.java
@@ -2,9 +2,11 @@ package com.rpatino12.epam.gym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GymApplication {
 

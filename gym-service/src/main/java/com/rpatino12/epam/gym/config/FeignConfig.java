61516c0afb4e3bc19f41a4acaf8c100896aa7a00
Ricardo Patino <rpatino12@outlook.com>
2024-05-21 17:33:53 -0500

    Implement Authorization - Bearer token for the secondary microservice (trainer-service) and the microservices integration now use JWT token implementation

---
+++ gym-service/src/main/java/com/rpatino12/epam/gym/config/FeignConfig.java
@@ -0,0 +1,29 @@
+package com.rpatino12.epam.gym.config;
+
+import feign.RequestInterceptor;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+    /**
+     * Feign is a declarative web service client, which makes writing HTTP clients easier.
+     * To pass the JWT token from this main microservice to any secondary microservice using Feign,
+     * you can add an interceptor that will add the JWT token to the request headers.
+     */
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+            String authorizationHeader = request.getHeader("Authorization");
+            if (authorizationHeader != null) {
+                requestTemplate.header("Authorization", authorizationHeader);
+            }
+        };
+    }
+}

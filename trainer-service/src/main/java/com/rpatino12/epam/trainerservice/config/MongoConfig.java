80300257fd5a77648fc93d53d3142b35d62062dc
Ricardo Patino <rpatino12@outlook.com>
2024-06-29 13:44:57 -0500

    Create 'without-db-and-queue' Spring profile for Reporting microservice

--- trainer-service/src/main/java/com/rpatino12/epam/trainerservice/config/MongoConfig.java
+++ trainer-service/src/main/java/com/rpatino12/epam/trainerservice/config/MongoConfig.java
@@ -4,6 +4,7 @@ import com.rpatino12.epam.trainerservice.model.Trainer;
 import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
@@ -13,6 +14,7 @@ public class MongoConfig {
     private MongoTemplate mongoTemplate;
 
     @PreDestroy
+    @Profile("!prod")
     public void dropCollections() {
         mongoTemplate.dropCollection(Trainer.class);
     }
